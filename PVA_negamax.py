57e120fd4d9b2e8271a1d5162af5250556ad47d2
jeet23 <jeet.23nz@gmail.com>
2017-10-23 23:12:40 +0100

    To make PVA work, made a new tree with re-ordering instead of each level re-ordering

--- PVA_negamax.py
+++ PVA_negamax.py
@@ -15,19 +15,18 @@ def negamax(root, branching, height, alpha, beta, Modified):
 	# Check if leaf node or Iterative deepening reached limit
 	if len(root.children) == 0 or heightChecked == iterativeDeepening:
 		global numberOfStaticEvaluation, numberOfStaticEvaluation_with_pva
-		numberOfStaticEvaluation += 1
-		# if Modified is True:
-		# 	numberOfStaticEvaluation_with_pva += 1
-		# else:
-		# 	numberOfStaticEvaluation += 1
+		if Modified:
+			numberOfStaticEvaluation_with_pva += 1
+		else:
+			numberOfStaticEvaluation += 1
 		PV = {}
 		return [PV, root.data]
 	else:
 		# Implement OrderMoves(root.children) here if parameter Modified == True
 		PV = {}
 		for move in range(0, branching):
-			if Modified is True:
-				root = orderMoves(root)
+			# if Modified is True:
+			# 	root = orderMoves(root)
 			newNode = root.children[move]
 			temp = -(negamax(newNode, branching, height - 1, -beta, -alpha, Modified))[1]
 			# Do nothing ---> destroy newNode
@@ -55,6 +54,20 @@ def orderMoves(root):
 	# print("Changed order : {} ".format([item.data for item in root.children]))
 	return root
 
+
+def reOrderTreePVA(root, height, branching):
+	if len(root.children) == 0:
+		return
+	minChild = root.children[0].data
+	for daughters in range(0, branching):
+		minChild = min(minChild, root.children[daughters].data)
+	array = [daughters.data for daughters in root.children]
+	minIndex = array.index(minChild)
+	root.children[0], root.children[minIndex] = root.children[minIndex], root.children[0]
+
+	for node in range(0, branching):
+		reOrderTreePVA(root.children[node], height-1, branching)
+
 def main():
 	# Input branching factor b , Height h and approximation approx
 
@@ -69,12 +82,19 @@ def main():
 	print("Adding root node at height {}: T Value: {}".format(h, tValue))
 	insertNodes(root, b, h, delta, approx, tValue)
 	printTree(root, b, h)
-	PV, negamaxValue = negamax(root, b, h, alpha, beta, True)
+	PV, negamaxValue = negamax(root, b, h, alpha, beta, False)
 	print("PV is {}: ".format(PV))
 	print("Negamax value is : {} ".format(negamaxValue))
 	print("numberOfStaticEvaluation is : {} ".format(numberOfStaticEvaluation))
 
 
+	reOrderTreePVA(root, h, b)
+	print("----------------------T R E E after reordering------------------")
+	printTree(root, b, h)
+	PV1, negamaxValue1 = negamax(root, b, h, alpha, beta, True)
+	print("PV is {}: ".format(PV1))
+	print("Negamax value is : {} ".format(negamaxValue1))
+	print("numberOfStaticEvaluation_with_pva is : {} ".format(numberOfStaticEvaluation_with_pva))
 
 main()
 
